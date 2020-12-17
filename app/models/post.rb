4cbfd14d97d389b28ddb2f9c6249e090b645bcc2
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-12-17 10:31:24 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
-class Post < ActiveRecord::Base
-  
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+  validates :name, presence: true
+  validates :content, presence: true
+
+  def tag_ids=(tags)
+    tags.each do |tag|
+      new_tag = Tag.find_or_create_by(id: tag)
+      post_tag = PostTag.create(post_id: self.id, tag_id: new_tag.id)
+      self.tags << new_tag
+    end
+  end
 end
